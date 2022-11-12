91d73b1c1673dece2dbf5506ff08345a1e9de8b1
edwinwmendez <mendez_ed@hotmail.es>
2022-11-11 23:05:16 -0500

    Cambio Usuario postgres

--- apiMiTox/apiMiTox/settings.py
+++ apiMiTox/apiMiTox/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db.apimitox',
-        'USER': 'postgres',
+        'USER': 'postgresql',
         'PASSWORD': 'Dr4g0n3k',
         'HOST': 'localhost',
         'PORT': '5432',
